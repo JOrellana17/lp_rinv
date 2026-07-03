d764a9b23d43a54d8e90b6a3811e1f288b8b0344
JOrellana17 <Jorgeorellana0045@gmail.com>
2026-07-02 19:17:57 -0600

    init

---
+++ src/data/pages/productos_page.js
@@ -0,0 +1,136 @@
+export default {
+    productos: [
+        {
+            sku: "0001",
+            nombre: "Auriculares inalámbricos ProSound X",
+            precio_unit: "79.99", cantidad_stock: 45,
+            categoria: "Electrónica"
+        },
+        {
+            sku: "0002",
+            nombre: "Smartwatch Active 3",
+            precio_unit: "129.00",
+            cantidad_stock: 28,
+            categoria: "Electrónica"
+        },
+        {
+            sku: "0003",
+            nombre: "Cámara deportiva 4K Ultra",
+            precio_unit: "199.00",
+            cantidad_stock: 12,
+            categoria: "Electrónica"
+        },
+        {
+            sku: "0004",
+            nombre: "Aspiradora vertical TurboClean 2000",
+            precio_unit: "149.50",
+            cantidad_stock: 8,
+            categoria: "Hogar"
+        },
+        {
+            sku: "0005",
+            nombre: "Cafetera Espresso Barista",
+            precio_unit: "89.90",
+            cantidad_stock: 20,
+            categoria: "Hogar"
+        },
+        {
+            sku: "0006",
+            nombre: "Juego de sábanas 200 hilos (King)",
+            precio_unit: "59.99",
+            cantidad_stock: 60,
+            categoria: "Hogar"
+        },
+        {
+            sku: "0007",
+            nombre: "Shampoo nutritivo 500ml",
+            precio_unit: "7.50",
+            cantidad_stock: 150,
+            categoria: "Aseo"
+        },
+        {
+            sku: "0008",
+            nombre: "Crema facial hidratante 50ml",
+            precio_unit: "12.00",
+            cantidad_stock: 80,
+            categoria: "Aseo"
+        },
+        {
+            sku: "0009",
+            nombre: "Protector solar FPS50 200ml",
+            precio_unit: "15.25",
+            cantidad_stock: 95,
+            categoria: "Aseo"
+        },
+        {
+            sku: "0010",
+            nombre: "Aceite de oliva extra virgen 1L",
+            precio_unit: "10.75",
+            cantidad_stock: 200,
+            categoria: "Alimentos"
+        },
+        {
+            sku: "0011",
+            nombre: "Café molido premium 500g",
+            precio_unit: "8.40",
+            cantidad_stock: 140,
+            categoria: "Alimentos"
+        },
+        {
+            sku: "0012",
+            nombre: "Miel orgánica 350g",
+            precio_unit: "6.30",
+            cantidad_stock: 75,
+            categoria: "Alimentos"
+        },
+        {
+            sku: "0013",
+            nombre: "Zapatillas running Velocity",
+            precio_unit: "69.99",
+            cantidad_stock: 34,
+            categoria: "Ropa"
+        },
+        {
+            sku: "0014",
+            nombre: "Chaqueta impermeable Trail",
+            precio_unit: "89.00",
+            cantidad_stock: 18,
+            categoria: "Ropa"
+        },
+        {
+            sku: "0015",
+            nombre: "Sudadera con capucha Classic",
+            precio_unit: "39.50",
+            cantidad_stock: 60,
+            categoria: "Ropa"
+        },
+        {
+            sku: "0017",
+            nombre: "Maletín para laptop 15''",
+            precio_unit: "55.00",
+            cantidad_stock: 10,
+            categoria: "Bolsos"
+        },
+        {
+            sku: "0018",
+            nombre: "Televisor LED 43'' Full HD",
+            precio_unit: "329.00",
+            cantidad_stock: 6,
+            categoria: "Electrónica"
+        },
+        {
+            sku: "0019",
+            nombre: "Barra de sonido 2.1 HomeSync",
+            precio_unit: "119.99",
+            cantidad_stock: 14,
+            categoria: "Electrónica"
+        },
+        {
+            sku: "0020",
+            nombre: "Set de cuchillos de cocina 6 piezas",
+            precio_unit: "34.95",
+            cantidad_stock: 40,
+            categoria: "Hogar"
+        }
+    ]
+};
